4c6a008dbab5e654c86fa1166040dcf1be939057
nickgois13-cell <nick.gois13@gmail.com>
2026-04-16 23:31:10 -0300

    Update ProductsSection.tsx

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -126,7 +126,7 @@ const ProductsSection = ({ onAddToCart }: ProductsSectionProps) => {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-7xl mx-auto">
           {filtered.map((product, i) => (
             <div
               key={product.id}
